752a63686a03c67ad35de6a8cff80842a36b0833
Gabriel Andrade <112653985+gandradex@users.noreply.github.com>
2025-01-31 11:05:07 -0600

    js exercise shopping list

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(text, paramTimes) {
 
+    let times = parseInt(paramTimes);
+    let textReturn = "";
+
+    // No negative numbers
+    if (times < 0) return 'ERROR';
+
+    for (let index = 0; index < times; index++) {
+        textReturn = textReturn + text;     
+    }
+
+    return textReturn;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+ 
